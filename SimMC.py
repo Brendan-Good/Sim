ddf11c63ca7f778528db8ee9a68feb92d5d52eab
Brendan Good <TheCombinatorax@gmail.com>
2013-07-12 15:48:30 -0400

    Fixed is_terminal/check_win in SimMC and SimMC_test

--- SimMC.py
+++ SimMC.py
@@ -41,29 +41,68 @@ def color_blue(Edge,Graph_Rep):
     Graph_Rep[2*m*Graph_Size-(m*(m+1))+2*n-2*m-1] = True
     return Graph_Rep
 
-def check_win(Color,Edges_to_Check,turn_number,past_iterations,win_subgraph):
-    ''' this takes in the most recent edge colored and the graph representation and checks to see if the colored graph now makes 
-    a k_n '''
-    colored_list = []
-    for edges in Edges_to_Check:
-        if(color(Edges)==Color):
-            colored_list.append(Edges)
-    if(len.colored_list<win_subgraph-past_iterations-1):
-        return false
-    else:
-        past_iterations += 1
-        if(past_iterations==win_subgraph):
-            print("Winning move") 
-            return true
-        else:
-            isterminal(Color,colored_list,turn_number,past_iterations,win_subgraph)
+#def check_win(Color,Edges_to_Check,turn_number,past_iterations,win_subgraph):
+#    ''' this takes in the most recent edge colored and the graph representation and checks to see if the colored graph now makes 
+#    a k_n '''
+#    colored_list = []
+#    for edges in Edges_to_Check:
+#        if(color(Edges)==Color):
+#            colored_list.append(Edges)
+#    if(len(colored_list)<win_subgraph-past_iterations-1):
+#        return false
+#    else:
+#        past_iterations += 1
+#        if(past_iterations==win_subgraph):
+#            print("Winning move") 
+#            return true
+#        else:
+#            isterminal(Color,colored_list,turn_number,past_iterations,win_subgraph)
              
 def is_terminal(Color,Edges_to_Check,turn_number,past_iterations,win_subgraph):
     if(turn_number==(n*(n-1))/2 or check_win(Color,Edges_to_Check,turn_number,past_iterations,win_subgraph)):
         return true
 
+#for is terminal take in an edge [x,y] and a list of colored edges that are of the color of the edge and if the length of this list is 
+# less than (l*(l-1))/2 then it is not terminal. Else find all of the red edges with x as an endpoint and store them.
+# If this is less than l-1, then no. else, 
+
+def check_win(Color,Edge,Edges_to_Check,win_subgraph):
+    colored_list = []
+    stored_vertices = [Edge[0]]
+    if (len(Edges_to_Check)<(win_subgraph*(win_subgraph-1))/2):
+        return False
+    else:
+        for edges in Edges_to_Check:
+            if(edges[0] == Edge[0]):
+                colored_list.append(edges)
+                stored_vertices.append(edges[1])
+            else:
+                if(edges[1]==Edge[0]):
+                    colored_list.append(edges)
+                    stored_vertices.append(edges[0])
+        print(stored_vertices)
+        if(len(colored_list)<win_subgraph-1):
+            return False
+        else:
+            Edges_to_Check_0 = []
+            for x in range(len(stored_vertices)-1):
+                for y in range(x,len(stored_vertices)):
+                    if(connect(stored_vertices[x],stored_vertices[y]) in Edges_to_Check):
+                        Edges_to_Check_0.append(connect(stored_vertices[x],stored_vertices[y]))
+            print(Edges_to_Check_0)
+            if(len(Edges_to_Check_0)<(win_subgraph*(win_subgraph-1))/2):
+                return False
+            else:
+                return True
+                        
+
+def connect(vertex1,vertex2):
+    edge = [min(vertex1,vertex2),max(vertex1,vertex2)]
+    return edge            
+            
+
+
+
 #The following two lines are to see if the code works as expected.
 color_blue([0,1],Graph_Rep)
 print(Graph_Rep.bin)
-       
-
