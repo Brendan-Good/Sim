ae49535c7ea83bf4616ec392ac01377ba4255c21
Tucker Bane <tuckerbane@snow-2.local>
2013-07-17 11:45:02 -0400

    Still working on Adj_maxtix, but it should still basically be in working order

--- Adj_Maxtix_Expand.py
+++ Adj_Maxtix_Expand.py
@@ -4,22 +4,22 @@ from bitstring import BitArray
 
 import copy
 import SimMCTest
+import itertools
 
 def Expand(graph):
-    ' ' ' Takes in a BitArray and returns a list of all possible positions after taking one move. abstract node types in graph["abst"]: 0 is the number of nodes with no colored edges, 1 is the number of isolated red edges, 2 is the number of isolated green/blue/2nd edges. Returns a list of graphs after distinct moves and a parallel list of metadata.' ' '
+    ' ' ' Takes in a graph dictionary and returns a list of all possible graphs
+    after taking one move. abstract node types in graph["abst"]: 0 is the number
+    of nodes with no colored edges, 1 is the number of isolated red edges, 2 is the
+    number of isolated green/blue/2nd edges. Returns a list of graphs after
+    distinct moves with an attached list of metadata (abstract node numbers).' ' '
 
     child_graphs = []
     new_graph = []
     for cat_num in range(0,len(graph['abst'])):
         for cat_num2 in range(cat_num,len(graph['abst'])):
-            #print(graph['abst'][cat_num])
-            #print(graph['abst'][cat_num2])
             if graph['abst'][cat_num] != 0 and graph['abst'][cat_num2] != 0 and(cat_num != cat_num2 or graph['abst'][cat_num]>1):
-               # print("if statment passed!")
-                print(cat_num)
-                print(cat_num2)
                 new_graph = copy.deepcopy(graph)
-                new_graph['abst'] = update_abst_nodes(new_graph['abst'],cat_num,cat_num2)
+                update_abst_nodes(new_graph,cat_num,cat_num2)
                 child_graphs.append(new_graph)
     
     
@@ -27,17 +27,50 @@ def Expand(graph):
     #print(child_graphs[0]['abst'])
     return child_graphs
 
-def update_abst_nodes(abst,cat1,cat2):
-  
+def update_abst_nodes(graph,cat1,cat2):
+    '''changes the number of abstract nodes of differant kinds and kicks
+    non-abstract nodes to the adj matrix '''
+    vert_num1 =-1
+    vert_num2 =-1
     if cat1 == 0 and cat2 == 0:
-        abst[0]-=2
-        abst[1]+=1
-    else:
-        abst[cat1]-=1 
-        abst[cat2]-=1
-    return abst
+        graph['abst'][0]-=2
+        graph['abst'][1]+=1
+    elif cat1 == 0:
+        graph['abst'][cat1]-=1
+        vert_num1 = kick_to_adj(graph,cat1) 
+        graph['abst'][cat2]-=1
+        vert_num2 = kick_to_adj(graph,cat2)
+        graph['adj'][vert_num1][vert_num2] = 1
     
+            
 
+def kick_to_adj(graph,abst_type):
+    '''Takes a graph and a kind of abstract structure and adds one
+    of that abstract structure to the adj matrix.
+    returns vert number of the first node kicked.
+    It may someday return the locations of the begining and end of the
+    abstract structure kicked if need be.'''
+   
+    scope = graph['scope']
+            
+    if abst_type ==0:
+        graph['scope']+=1
+        vert_num = graph['scope']
+
+    elif abst_type ==1:
+        graph['adj'][scope+1][scope+2] =1
+        graph['scope']+=2
+        vert_num = graph['scope']-1
+
+    elif abst_type ==2:
+        graph['adj'][scope+1][scope+2] =-1
+        graph['scope']+=2
+        vert_num = graph['scope']-1
+
+    return vert_num
+
+#def adj_update(graph,real_coords,abst_type1,abst_type2):
+    
     
     
 
