e0091c02800f325034c2b8e5704e12aa91d67734
Tucker Bane <tuckerbane@snow-2.local>
2013-07-19 13:59:49 -0400

    commit message

--- negamax_sim.py
+++ negamax_sim.py
@@ -3,41 +3,60 @@
 #Negamax implementation by Joshua Brule
 
 import sys
-import Sim
-import SimMCTest
+import Adj_Maxtix_Expand
+import is_terminal
 
 from collections import deque
 from copy import deepcopy
 
 N = 7
 M = 8
-MAX_DEPTH = 4
+MAX_DEPTH = 15
 
 inf = float('inf')
+
+def play_game()
+    adj = [[0 for col in range(6)] for row in range(6)]
+    abst = [6,0,0]
+    tuples = is_terminal.generate_structure(6,3)
+    scope = -1
+    depth = 1
+    graph = {'adj':adj,'abst':abst,'tuples':tuples,'scope':scope,'depth':depth}
     
-def negamax(node, alpha, beta, player, depth = 15):
+    game_over = False
+
+    while (!game_over):
+        move = negamax(graph,-inf,+inf,1,15)
+
+        game_over = is_terminal.is_terminal(graph['tuples'],edge)
+
+    
+
+
+def negamax(node, alpha, beta, player, depth):
     '''Perform a depth limited negamax search using functions eval(), expand()
     and isTerminal() as appropriate
     
     Returns (optimal move, negamax value)
     
-    Usage: negamax(new_board(), MAX_DEPTH, -inf, +inf, 1)'''
+    Usage: negamax(new_board(), -inf, +inf, 1, MAX_DEPTH)'''
     
 
-    if is_terminal(node):
-        return (None, 1)
+    if is_termial.is_terminal(node):
+        return (None, +inf)
     elif depth == 0:
         return(None,0)
-    
+
     else:
         candidate_move = None
-        for move in Sim.Expand(node):
+        for move in Adj_Maxtox.Expand.Expand(node):
             val = -1 * negamax(move.reverse(0,len(move)), depth - 1, -beta, -alpha, -player)[1]
             if val >= beta:
                 return (None, val)
             if val > alpha:
-                candidate_move = move_extractor(move,node)
+                candidate_move = move
                 alpha = val
+
 
         return (candidate_move, alpha)
 # method by Tucker Bane
